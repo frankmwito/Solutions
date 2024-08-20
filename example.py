dda4791b3e59253f7e80abc37d6cca3d27fd6636
Frank Mwito <Mfranklyne039@gmail.com>
2024-08-20 23:04:44 +0300

    first commit

---
+++ example.py
@@ -0,0 +1,10 @@
+def add_numbers():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    
+    sum = num1 + num2
+    print("Python is versatile! \nI am learning Python!")
+    print("The sum is:", sum)
+
+# Call the function
+add_numbers()
